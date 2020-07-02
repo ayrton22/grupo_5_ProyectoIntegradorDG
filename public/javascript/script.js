9ae30ddca86b8e2358c9c076d7f8ce06b813f092
Ayrton <themasterayrx@hotmail.com>
2020-07-02 09:26:13 -0300

    Hice una pavada de prueba en un boton de registro

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,119 +1,49 @@
-let videoIds = ["JSapXD9vxYA", "vgQNOIhRsV4", "oz-O74SmTSQ", "ZOgBVR21pWg", "GTRRJkfo5Xs"];
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
-Vue.component('video-carousel', {
-	data: function() {
-		return {
-			currentVideo: 0, //refers to tertiary video on leftmost; main video with be currentVideo+2
-			totalVideos: 8,
-			videos: [],
-			videoPositions: {
-				0 : "tertiary tertiary-left",
-				1 : "secondary secondary-left",
-				2 : "main",
-				3 : "secondary secondary-right",
-				4 : "tertiary tertiary-right"
-			}
-		}
-	},
-	computed: {
-		
-	},
-	methods: {
-		changeVideo: function(direction, position) {
-			if(direction == "left") {
-				this.currentVideo--;
-				if(this.currentVideo < 0) {
-					this.currentVideo = 4;
-				}
-				
-			} else if (direction == "right") {
-				this.currentVideo++;
-				if(this.currentVideo > 4) {
-					this.currentVideo = 0;
-				}
-			} else {
-				switch(position) {
-					case 'secondary secondary-right':
-						this.changeVideo('left');
-						break;
-					case 'secondary secondary-left':
-						this.changeVideo('right');
-						break;
-					case 'tertiary tertiary-right':
-						this.changeVideo('left');
-						this.changeVideo('left');
-						break;
-					case 'tertiary tertiary-left':
-						this.changeVideo('right');
-						this.changeVideo('right');
-						break;
-				}
-			}
-			
-			this.videos.forEach((video, i)=>{
-				video.active = false;
-				let newIndex = i+this.currentVideo;
-				if(newIndex > 4) {
-					newIndex %= 5;
-				}
-				video.position = this.videoPositions[newIndex];
-			});
-			
-		}
-	},
-	mounted: function() {
-		//Generate array of fake carousel videos
-		for (i=0; i<5; i++) {
-			let videoId = videoIds[i];
-			let videoEmbed = "https://www.youtube.com/embed/" + videoId + "?autoplay=1";
-			let videoImage = "https://img.youtube.com/vi/" + videoId + "/maxresdefault.jpg";
-			let channelImage = "https://yt3.ggpht.com/a-/AAuE7mCXgsNVSlp9-BLuKFoaAqKxpjJ0T1NB1yJzdQ=s288-mo-c-c0xffffffff-rj-k-no";
-			let video = {
-				thumbnail: videoImage,
-				embed: videoEmbed,
-				position: this.videoPositions[i],
-				videoId: videoId,
-				active: false,
-				channelImage: channelImage,
-				channelName: "New Game Plus",
-				channelTitle: "Running through some sweet Ninty covers",
-				channelViews: 2345
-			}
-			
-			this.videos.push(video);
-		}
-	}
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
 });
 
-let app = new Vue({
-	el: '#container',
-	data: {
-		users: []
-	},
-	methods: {
-		
-	},
-	mounted: function() {
-		let self = this;
-		
-		// Generate array of fake users
-		for(let i=0;i<15; i++) {
-			//generate random user
-			let user = {
-				imageLink: faker.internet.avatar(),
-				username: faker.internet.userName(),
-				playing: faker.company.companyName(),
-				playingDisplay: null,
-				viewers: chance.integer({min: 0, max: 999}),
-				id: null
-			}
-			user.id = user.username;
-			if(user.playing.length > 20) {
-				user.playingDisplay = user.playing.slice(0,20) + "...";
-			} else {
-				user.playingDisplay = user.playing;
-			}
-			self.users.push(user);
-		}
-	}
-});
+function show_hide(){
+    let click = document.getElementById('drop-cont');
+    let click2 = document.getElementById('drop-cont2');
+    if(click.style.display === 'none'){
+        click.style.display = 'block';
+    } else {
+        click.style.display = 'none';
+    }
+
+    if(click2.style.display = 'block'){
+        click2.style.display = 'none';
+    }
+}
+
+function show_hide2(){
+    let click = document.getElementById('drop-cont2');
+    let click2 = document.getElementById('drop-cont');
+    if(click.style.display === 'none'){
+        click.style.display = 'block';
+    } else {
+        click.style.display = 'none';
+    }
+
+    if(click2.style.display = 'block'){
+        click2.style.display = 'none';
+    }
+}
+
+function containerHide(){
+    let click = document.getElementById('drop-cont');
+    if (click.style.display === 'block'){
+        click.style.display = 'none'
+    }
+
+    let click2 = document.getElementById('drop-cont2');
+    if (click2.style.display === 'block'){
+        click2.style.display = 'none'
+    }
+}

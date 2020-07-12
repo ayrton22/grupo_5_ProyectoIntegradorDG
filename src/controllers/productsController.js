8034ffcb28ab72b0efe4aec392bf27fcb071504e
Joaquin <joaquiperles@live.com.ar>
2020-07-12 20:58:39 -0300

    crear producto

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,88 +1,80 @@
 const fs = require('fs');
 const path = require('path');
 
-let producto = fs.readFileSync(path.join(__dirname, '../data/products.json'), 'utf8');
-producto = JSON.parse(producto);
+let productos = fs.readFileSync(path.join(__dirname, '../data/products.json'), 'utf8');
+productos = JSON.parse(productos);
 
 module.exports = {
     allProducts: (req, res) => {
 		res.render('productos', {
-			producto: producto
+			producto: productos
 		})
 	},
+
     shoppingCart: function(req, res){
         res.render('carritoDeCompras')
-    },
-    register: function(req, res) {
-        res.render('registro');
-    },
-    signIn: function(req, res) {
-        res.render('login');
-    },
-    productLoad: function(req, res) {
-        res.render('cargaDeProductos');
-    },
-    detail: function(req, res) {
-        let idProducto = req.params.productId;
-		for (let i = 0; i < producto.length; i++) {
-			if (producto[i].id == idProducto) {
-				res.render('detalleProducto', {
-					productoDetalle: producto[i]
-				})
-			}
-		}
-    },
-    edit: (req, res) => {
-		for (let i = 0; i < producto.length; i++) {
-			if (producto[i].id == req.params.productId) {
-				res.render('product-edit-form', {
-					producto: producto[i]
-				})
-			}
-		}
-    },
-	update: (req, res) => {
-		let productoActualizado = {
-			id: Number(req.params.productId),
-			...req.body
-		}
-		for (let i = 0; i < producto.length; i++) {
-			if (producto[i].id == productoActualizado.id) {
-				producto[i] = productoActualizado;
-				fs.writeFileSync(path.join(__dirname, '../data/products.json'), JSON.stringify(producto));
-				res.redirect('/products/detail/' + producto.id)
-			}
-		}
 	},
-    destroy: (req, res) => {
-		for (let i = 0; i < producto.length; i++) {
-			if (producto[i].id == req.params.productId) {
-				let index = producto.indexOf(producto[i]);
-				producto.splice(index, 1);
-				fs.writeFileSync(path.join(__dirname, '../data/products.json'), JSON.stringify(producto));
-				res.redirect('/products?status=ok')
+	
+	detail: function(req, res) {
+		let idProducto = req.params.id;
+		for (let i = 0; i < productos.length; i++) {
+			if (productos[i].id == idProducto) {
+				res.render('detalleProducto', {
+					productoDetalle: productos[i]
+				})
 			}
 		}
 	},
 
-    productLoad: function(req, res) {
-        res.render('cargaDeProductos');
-    },
-    productUpdate: function(req, res) {
-        let nuevoProducto = {
+    load: function(req, res) {
+		res.render('cargaDeProductos')
+	},
+
+	store: function(req, res) {
+		let nuevoProducto = {
             id: productos.length + 1,
-            tittle: req.body.nombre,
-            statement: req.body.estado,
-            plataform: req.body.plataforma,
-            category: req.body.categoria,
-            picture: req.body.foto,
-            pagyFomr:  req.body.pagos,
-            delivery:  req.body.entrega,
-            price:  req.body.precio
+            title: req.body.title,
+            description: req.body.description,
+            /*medium-description: req.body.username,
+            big-description: req.body.email,*/
+			price: `$${req.body.price}`,
+			image: req.body.image,
+			video: req.body.video,
+			editora: req.body.editora,
+			lanzamiento: req.body.lanzamiento,
+			desarrolladora: req.body.desarrolladora,
+			etiquetas: req.body.etiquetas,
+			clasificacion: req.body.clasificacion,
+			genre: req.body.genre,
+			category: req.body.category,
+			rating: req.body.rating
         };
-        productos.push(nuevoProducto);
-        fs.writeFileSync(path.join(__dirname, '../data/products.json'), JSON.stringify(productos));
-        res.redirect('/')
-        res.render();
-    }
+        for(let i = 0; i < productos.length; i++) {
+            if(req.body.title == productos[i].title || req.body.image == productos[i].image || req.body.description == productos[i].description) {
+                return res.redirect('/product/load');
+            }
+        }
+		productos.push(nuevoProducto);
+		fs.writeFileSync(path.join(__dirname, '../data/products.json'), JSON.stringify(productos))
+		res.redirect('/')
+    },
+	
+    edit: function(req, res) {
+		res.render('productEdit')
+	},
+	
+	update: function(req, res) {
+
+	},
+
+    destroy: (req, res) => {
+		for (let i = 0; i < productos.length; i++) {
+			if (productos[i].id == req.params.id) {
+				let index = productos.indexOf(productos[i]);
+				productos.splice(index, 1);
+				fs.writeFileSync(path.join(__dirname, '../data/products.json'), JSON.stringify(productos));
+				res.redirect('/')
+			}
+		}
+	}
 }

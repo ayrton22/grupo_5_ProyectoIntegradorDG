16371a96d5f3c73ce78f3cff94cd180944c25a8c
Joaquin <joaquiperles@live.com.ar>
2020-07-19 18:24:19 -0300

    Validacion de Registro

--- src/routes/users.js
+++ src/routes/users.js
@@ -2,10 +2,12 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
+const registerValidation = require('../validations/registerValidation');
+
 const controller = require('../controllers/userController')
 
 router.get('/register', controller.register);
-router.post('/register', controller.save);
+router.post('/register', registerValidation, controller.save);
 
 router.get('/edit/:id?', controller.edit);
 router.put('/edit/:id', controller.update);
---
+++ src/validations/registerValidation.js
@@ -0,0 +1,31 @@
+const {check, validationResult, body} = require('express-validator');
+
+module.exports = [
+    check('name')
+        .isAlpha()
+            .withMessage('Tu nombre no puede tener números ni caracteres especiales')
+        .isLength({max: 50})
+            .withMessage('Tu nombre no puede tener números ni caracteres especiales, y un maximo de 50 caracteres'),
+
+    check('surname')
+        .isAlpha()
+            .withMessage('Tu apellido no puede tener números ni caracteres especiales')
+        .isLength({max: 50})
+            .withMessage('Tu apellido no puede tener más de 50 caracteres'),
+
+    check('username')
+        .isLength({min: 5, max: 20})
+            .withMessage('Tu nombre de usuario tiene que tener entre 5 y 20 caracteres'),
+        
+    check('email')
+        .isEmail()
+            .withMessage('Tenés que insertar un email válido'),
+            
+    check('password')
+        .isLength({min: 6, max: 20})
+            .withMessage('Como mínimo la contraseña debe tener 4 caracteres. Como máximo 16'),
+
+    check('repassword')
+        .matches('password')
+            .withMessage('Las contraseñas no coinciden')
+]

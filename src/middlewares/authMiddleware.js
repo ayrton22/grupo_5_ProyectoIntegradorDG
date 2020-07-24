765380a97d1428edcb48a42cfb81f116451b1ab5
Ayrton <themasterayrx@hotmail.com>
2020-07-24 12:04:35 -0300

    validacion de usuario registrado terminado

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -1,3 +1,8 @@
+const fs = require('fs');
+const path = require('path');
+let usuarios = fs.readFileSync(path.join(__dirname, '../data/users.json'), 'utf8');
+usuarios = JSON.parse(usuarios);
+
 function authMiddleware(req, res, next){
     if(req.session.usernameUser == undefined){
         return res.redirect('/user/login')

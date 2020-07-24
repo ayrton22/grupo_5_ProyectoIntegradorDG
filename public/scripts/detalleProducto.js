f1db743e464d65cc5a5dcb9351568e867666c3f1
elimerj <elimer_duarte@outlook.com>
2020-07-24 09:00:15 -0300

    slice de video e imagenes terminado, si se agregan mas videos, se cambian las funciones, trabajando en los efectos finales.

--- public/scripts/detalleProducto.js
+++ public/scripts/detalleProducto.js
@@ -4,7 +4,9 @@ const fila = document.querySelector('.contenedor-carrusel');
 const imagenes = document.querySelectorAll('.imagen-gameplay img');
 const flechaIzquierda = document.getElementById('flecha-izquierda');
 const flechaDerecha = document.getElementById('flecha-derecha');
-let contador = 0;
+const vistaVideo = document.querySelector('.vista iframe');
+const vistaImg = document.querySelector('.vista img');
+const logo = document.querySelector('.logo');
 
 // Event Listener para la flecha derecha //
 
@@ -16,22 +18,39 @@ flechaIzquierda.addEventListener('click', () => {
     fila.scrollLeft -= fila.offsetWidth;
 });
 
-setInterval(()=>{
+/*setInterval(()=>{
     fila.scrollLeft += fila.offsetWidth;
-},10000)
+},10000)*/
 
 //carousel imagen script
+
+
 imagenes.forEach(img => img.addEventListener('click', imgClick));
 
 function imgClick(e) {
-    //fila.style.transform = 'translate(-100%)'
-
+    //deleting border
+    imagenes.forEach(img => (img.classList.remove('bordeado')));
+    //switch mode video-img
+    if(e.target.classList.contains('videoPreview')){
+        vistaVideo.classList.remove('oculto');
+        vistaImg.classList.add('oculto');
+        logo.classList.add('oculto');
+    }
+    else{
+    vistaImg.classList.remove('oculto');
+    vistaVideo.classList.add('oculto');
+    logo.classList.remove('oculto');
+    }
     //change the imagen to src of clicked image
     current.src = e.target.src;
-    
+
     //add fade in class
     current.classList.add('fade-in');
+
     //remove fade-in class
     setTimeout(()=> current.classList.remove('fade-in'),500);
+    //border for active imagen element
+    e.target.classList.add('bordeado');
 }
+
 

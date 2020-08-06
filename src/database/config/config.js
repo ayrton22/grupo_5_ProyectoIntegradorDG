f43d39c568b2363217a69d9d380196935689ee3f
Joaquin <joaquiperles@live.com.ar>
2020-08-06 18:34:07 -0300

    Introduccion a Sequelize

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,8 +1,8 @@
-{
+module.exports = {
   "development": {
     "username": "root",
     "password": null,
-    "database": "database_development",
+    "database": "house_of_games",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },

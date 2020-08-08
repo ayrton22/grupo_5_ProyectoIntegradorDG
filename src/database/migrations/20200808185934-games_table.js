0c63e9642d9b9448fc5532fdd6eee2b71d9f73b8
benjabarreiro <barreirobenjamin@gmail.com>
2020-08-08 18:19:37 -0300

    migration games hecha

--- src/database/migrations/20200808185934-games_table.js
+++ src/database/migrations/20200808185934-games_table.js
@@ -2,20 +2,83 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    return queryInterface.createTable('games', {
+      id: {
+        type: Sequelize.DataTypes.INTEGER(100).UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title: {
+        type: Sequelize.DataTypes.STRING(60),
+        allowNull: false,
+        unique: true
+      },
+      description: {
+        type: Sequelize.DataTypes.STRING(100),
+        allowNull: false,
+      },
+      descriptionMedium: {
+        type: Sequelize.DataTypes.STRING(200),
+        allowNull: false,
+      },
+      descriptionBig: {
+        type: Sequelize.DataTypes.STRING(400),
+        allowNull: false,
+      },
+      price: {
+        type:Sequelize.DataTypes.DECIMAL(5000, 2),
+        allowNull: false
+      },
+      createdAt: Sequelize.DataTypes.DATE,
+      updatedAt: Sequelize.DataTypes.DATE,
+      video: {
+        type: Sequelize.DataTypes.STRING(100),
+        allowNull: false
+      },
+      launch_date: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: false
+      },
+      editor: {
+        type: Sequelize.DataTypes.INTEGER(50),
+        allowNull: false
+      },
+      clasification: {
+        type: Sequelize.DataTypes.STRING(10),
+        allowNull: false
+      },
+      rating: {
+        type: Sequelize.DataTypes.DECIMAL(10, 2),
+        allowNull: false
+      },
+      stock: {
+        type: Sequelize.DataTypes.INTEGER(100),
+        allowNull: false
+      },
+      genre: {
+        type: Sequelize.DataTypes.INTEGER(100).UNSIGNED,
+        foreignKey: true,
+        autoIncrement: true
+      },
+      category: {
+        type: Sequelize.DataTypes.INTEGER(100).UNSIGNED,
+        foreignKey: true,
+        autoIncrement: true
+      },
+      platform: {
+        type: Sequelize.DataTypes.INTEGER(100).UNSIGNED,
+        foreignKey: true,
+        autoIncrement: true
+      },
+      img: {
+        type: Sequelize.DataTypes.INTEGER(100).UNSIGNED,
+        foreignKey: true,
+        autoIncrement: true
+      }
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    return queryInterface.dropTable('games')
   }
 };

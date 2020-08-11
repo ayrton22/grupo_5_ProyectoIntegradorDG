b88aea4718dfcab21049f356ed5cda5e8ba10263
benjabarreiro <barreirobenjamin@gmail.com>
2020-08-11 12:24:39 -0300

    agregué el seeder de games

---
+++ src/database/seeders/20200808211156-games.js
@@ -0,0 +1,28 @@
+'use strict';
+const faker = require('faker');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('games', [
+      {
+        title: faker.name.title(),
+        description: faker.lorem.sentence(),
+        descriptionMedium: faker.lorem.sentence(),
+        descriptionBig: faker.lorem.paragraph(),
+        price: faker.commerce.price(),
+        //created_at: new Date(),
+        //updated_at: new Date(),
+        video: faker.internet.url(),
+        //launch_date: faker.date.between('1900-01-01, 2020-01-01'),
+        editor: faker.lorem.word(),
+        clasification: faker.lorem.word(),
+        rating: faker.random.number(),
+        stock: faker.random.number()
+      }
+    ])
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('games', null, {})
+  }
+};

cd6d91fd350f4a1873147c6272549162946cafd5
elimerj <elimer_duarte@outlook.com>
2020-08-12 17:22:47 -0300

    agregue los modelos faltantes

---
+++ src/database/migrations/20200812201020-message.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.createTable('message', {
+      id: {
+        type: Sequelize.DataTypes.INTEGER(100).UNSIGNED,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      id_user: {
+        type: Sequelize.DataTypes.INTEGER(100).UNSIGNED,
+        allowNull: false,
+        references: {
+          model: 'User.js',
+          key: 'id'
+        }
+      },
+      message: {
+        type: Sequelize.DataTypes.STRING(500),
+        allowNull: false
+      },
+      createdAt: Sequelize.DataTypes.DATE,
+      updatedAt: Sequelize.DataTypes.DATE
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('message');
+  }
+};

f525e398d72467e73c5d72313c1269eceb2e1d79
Ayrton <themasterayrx@hotmail.com>
2020-08-18 17:01:19 -0300

    arreglando cosas

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -127,13 +127,23 @@ module.exports = {
 				}
 				db.Games_Genres.bulkCreate(genresGame)
 				.then(() => {
-						db.Games_Categories.create({
-							id_game: gameId, id_category: Number(req.body.category)
-						})
+					let categoryGame = [];
+					for(let i = 0; i < req.body.category.length; i++){
+					categoryGame.push({
+						id_game: gameId,
+						id_category: Number(req.body.category[i]),
+					})
+					}
+					 	db.Games_Categories.bulkCreate(categoryGame)
 						.then(() => {
-						   db.Games_Platforms.create({
-								id_game: gameId, id_platform: Number(req.body.platform)
-						   })
+							let platformGame = [];
+							for(let i = 0; i < req.body.platform.length; i++){
+								platformGame.push({
+									id_game: gameId,
+									id_platform: Number(req.body.platform[i]),
+								})
+							}
+						    db.Games_Platforms.bulkCreate(platformGame)
 						})
 				})
 			})
@@ -163,50 +173,6 @@ module.exports = {
 
 		let url = new URL(req.body.video);
 		let videoCode = new URLSearchParams(url.search).get("v");
-
-		let editedProduct = {
-            id: req.params.id,
-            title: req.body.title,
-            description: req.body.description,
-            medium_description: req.body.medium_description,
-            big_description: req.body.big_description,
-			price: req.body.price,
-			image: productToEdit.image,
-			imagen_horizontal: productToEdit.imagen_horizontal,
-			image1: productToEdit.image1,
-			image2: productToEdit.image2,
-			image3: productToEdit.image3,
-			image4: productToEdit.image4,
-			image5: productToEdit.image5,
-			image6: productToEdit.image6,
-			image7: productToEdit.image7,
-			image8: productToEdit.image8,
-			image9: productToEdit.image9,
-			image10: productToEdit.image10,
-			imagen_detalle: productToEdit.imagen_detalle,
-			video: videoCode,
-			editor: req.body.editor,
-			launch_date: req.body.launch_date,
-			developer: req.body.developer,
-			tags: req.body.tags,
-			classification: req.body.classification,
-			category: productToEdit.category,
-			rating: req.body.rating,
-			playstation: (req.body.plataformPlay == 'on') ? 'si' : 'no',
-			xbox: (req.body.plataformXbox == 'on') ? 'si' : 'no',
-			pc: (req.body.plataformPc == 'on') ? 'si' : 'no',
-			icon_playstation: productToEdit.icon_playstation,
-			icon_xbox: productToEdit.icon_xbox,
-			icon_pc: productToEdit.icon_pc
-        };
-        
-        for(let i = 0; i < products.length; i++) {
-            if(products[i].id == req.params.id ) {
-                products[i] = editedProduct;
-                fs.writeFileSync(path.join(__dirname, '../data/products.json'), JSON.stringify(products))
-                return res.redirect('/product/detail/' + editedProduct.id)
-            }
-        }
 	},
 
     destroy: (req, res) => {

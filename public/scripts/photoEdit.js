848fc9642bcf8344c2887b813e99644f4b9d0406
Joaquin <joaquiperles@live.com.ar>
2020-08-22 17:48:50 -0300

    ww

--- public/scripts/photoEdit.js
+++ public/scripts/photoEdit.js
@@ -7,30 +7,22 @@ window.addEventListener('load', function() {
     let portrayPhoto = qs('img.productEdit-form-uploadImage--portray');
     let horizontalPhoto = qs('img.productEdit-form-uploadImage--horizontal');
     let detailPhoto = qs('img.productEdit-form-uploadImage--detail');
-    let image1 = qs('img#one');
-    let image2 = qs('img#two');
-    let image3 = qs('img#three');
-    let image4 = qs('img#four');
-    let image5 = qs('img#five');
-    let image6 = qs('img#six');
-    let image7 = qs('img#seven');
-    let image8 = qs('img#eight');
-    let image9 = qs('img#nine');
-    let image10 = qs('img#ten');
+
+    let imagesArray = document.getElementsByName('carousel');
+
+    let imagesArray = document.getElementsByName('carouselZoom');
+
+    let inputsArray = document.getElementsByName('carouselInput');
+
+    let crossArray = document.getElementsByName('carouselZoomCross');
+
+    let btnArray = document.getElementsByName('carouselZoomBtn');
+
+    let generalZoomArray = document.getElementsByClassName('inactive-single-image-container');
 
     let portrayInput = qs('input#portray');
     let horizontalInput = qs('input#horizontal');
     let detailInput = qs('input#detail');
-    let image1Input = qs('input#one');
-    let image2Input = qs('input#two');
-    let image3Input = qs('input#three');
-    let image4Input = qs('input#four');
-    let image5Input = qs('input#five');
-    let image6Input = qs('input#six');
-    let image7Input = qs('input#seven');
-    let image8Input = qs('input#eight');
-    let image9Input = qs('input#nine');
-    let image10Input = qs('input#ten');
 
     let portrayZoom = qs('div#portrayZoom');
     let portrayCross = qs('div#portrayZoom i');
@@ -44,6 +36,7 @@ window.addEventListener('load', function() {
     let detailCross = qs('div#detailZoom i');
     let detailBtn = qs('div#detailZoom button');
 
+    console.log(imagesArray);
 
     portrayPhoto.addEventListener('click', function() {
 
@@ -101,6 +94,7 @@ window.addEventListener('load', function() {
             detailZoom.classList.add('active_detail');
 
             detailCross.addEventListener('click', function() {
+
                 detailZoom.classList.remove('active_detail');
                 detailZoom.classList.add('inactive_detail');
             });
@@ -116,5 +110,53 @@ window.addEventListener('load', function() {
             detailInput.click();
         };
     });
+
+    imagesArray.forEach(function(element) {
+        element.addEventListener('click', function() {
+
+            if (element.dataset.status == "active") {
+                
+                generalZoomArray.forEach(function(subElement) {
+                    
+                    subElement[element].classList.remove('inactive-single-image-container');
+                    subElement[element].classList.add('active-single-image-container');
+                    
+                });
+
+                crossArray.forEach(function(subElement) {
+                    
+                    generalZoomArray.forEach(function(subElement) {
+                    
+                        subElement[element].classList.remove('inactive-single-image-container');
+                        subElement[element].classList.add('active-single-image-container');
+                        
+                    });    
+
+                });
+
+                btnArray.forEach(function(subElement) {
+                    
+                    element.dataset.status = "inactive";
+                    element.src = "/img/Logos/Photo-upload.jpg"
+
+                    generalZoomArray.forEach(function(subElement) {
+                    
+                        subElement[element].classList.remove('inactive-single-image-container');
+                        subElement[element].classList.add('active-single-image-container');
+                        
+                    });    
+                    
+                })
+
+            } else {
+
+                inputsArray.forEach(function(subElement) {
+                    subElement[element].click()
+                })
+
+            };
+
+        })
+    })
 
 })
